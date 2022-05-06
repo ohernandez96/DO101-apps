f34b752ff431e6585461709d5899b165d5408e0a
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 02:29:51 -0400

    3rd build

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,31 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  var choice, course, coursesEnrolled;
-  coursesEnrolled = set();
-
-  while (true) {
-    console.log("Press 1 to Add course\n2 to Drop Course\n3 To show list of registered Courses\n4 to Exit. ");
-    choice = input("\nEnter your number: ");
-
-    if (choice === "1") {
-      course = input("Enter the name of the course: ");
-      coursesEnrolled.add(course);
-  }
-
-    if (choice === "2") {
-      course = input("Enter the course you wish to drop: ");
-      coursesEnrolled.remove(course);
-    }
-
-    if (choice === "3") {
-      console.log("Registered courses: ", coursesEnrolled);
-  }
-
-    if (choice === "4") {
-      break;
-  }
-}
+  res.send('You have decided to roll a d20 dice, and you recieved: ');
 });
 
 app.listen(8080, function () {
--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,25 @@
-console.log("Hello World!\n");
+var choice, course, coursesEnrolled;
+coursesEnrolled = set();
 
+while (true) {
+    console.log("Press 1 to Add course\n2 to Drop Course\n3 To show list of registered Courses\n4 to Exit. ");
+    choice = input("\nEnter your number: ");
+
+    if (choice === "1") {
+        course = input("Enter the name of the course: ");
+        coursesEnrolled.add(course);
+    }
+
+    if (choice === "2") {
+        course = input("Enter the course you wish to drop: ");
+        coursesEnrolled.remove(course);
+    }
+
+    if (choice === "3") {
+        console.log("Registered courses: ", coursesEnrolled);
+    }
+
+    if (choice === "4") {
+        break;
+    }
+}
