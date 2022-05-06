24697a7d7f72f035412ac780358995138dcee1d9
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 03:37:17 -0400

    18

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,10 +4,17 @@ app = express();
 app.get('/', function (req, res) {
   res.type('text/plain');
   var randnumber = Math.floor(Math.random() * 20) + 1;
-  if (randnumber > 10) {
-    var prompt = "You pass!"
-  } else {
-    var prompt = "Oof, sorry. You failed."
+  if (randnumber == 1) {
+    var prompt = "You got the lowest possible roll. You failed. Better luck next time! \n Refresh to roll again"
+  }
+  else if (randnumber < 11) {
+    var prompt = "Sorry, you failed. Refresh to roll again!"
+  }
+  else if (randnumber < 20) {
+    var prompt = "You passed! Refresh to roll again."
+  }
+  else {
+    var prompt = "You got the highest possible roll! Congrats! \n Refresh to roll again."
   }
 
   res.send('You have decided to roll a D20 dice.\nIf you get 10 or less, you fail. Otherwise, you pass. And you got a...\n'+randnumber+'!'+
