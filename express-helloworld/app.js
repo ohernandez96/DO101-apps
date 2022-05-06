5c1b21021e4229cade9289fb487795145fa1f29f
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 02:39:56 -0400

    5th

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  console.log('You have decided to roll a d20 dice, and you recieved: ');
+  res.send('You have decided to roll a d20 dice, and you recieved: ');
   var b = Math.random();
-  console.log('\n',b);
+  res.send('\n',b);
 
 });
 
