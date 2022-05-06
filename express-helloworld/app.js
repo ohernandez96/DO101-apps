c10ff516b55ca943882452705ea12eea5d1370d0
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 02:57:44 -0400

    10

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -3,8 +3,8 @@ app = express();
 
 app.get('/', function (req, res) {
   const b = Math.random();
-  console.log("You have decided to roll a D20 dice and you got...\n")
-  res.send(b);
+  res.send("You have decided to roll a D20 dice and you got...\n",
+  console.log(b));
 
 });
 
