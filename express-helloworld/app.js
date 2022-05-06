d23553689b0339558f9ebb62567d17579ff1d8dd
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 02:21:48 -0400

    2nd build

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,31 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  var choice, course, coursesEnrolled;
+  coursesEnrolled = set();
+
+  while (true) {
+    console.log("Press 1 to Add course\n2 to Drop Course\n3 To show list of registered Courses\n4 to Exit. ");
+    choice = input("\nEnter your number: ");
+
+    if (choice === "1") {
+      course = input("Enter the name of the course: ");
+      coursesEnrolled.add(course);
+  }
+
+    if (choice === "2") {
+      course = input("Enter the course you wish to drop: ");
+      coursesEnrolled.remove(course);
+    }
+
+    if (choice === "3") {
+      console.log("Registered courses: ", coursesEnrolled);
+  }
+
+    if (choice === "4") {
+      break;
+  }
+}
 });
 
 app.listen(8080, function () {
