deb16a50db220ea9ee0ff5f7c55d640868d42433
ohernandez96 <hernand.oswaldo@gmail.com>
2022-05-06 02:36:56 -0400

    4th changee

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('You have decided to roll a d20 dice, and you recieved: ');
-  const b = Math.random();
+  console.log('You have decided to roll a d20 dice, and you recieved: ');
+  var b = Math.random();
   console.log('\n',b);
+
 });
 
 app.listen(8080, function () {
